9b62d2fb201ef7ff53adc170c8b46af72e074d69
Decrypted707 <72243894+Decrypted707@users.noreply.github.com>
2023-06-07 08:50:08 +0800

    Changes

---
+++ src/qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"647eea65098f070019962fe4",
+				"647eea65098f070019962fe7"
+			]
+		},
+		"647eea65098f070019962fe4": {
+			"id": "647eea65098f070019962fe4",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"647eea65098f070019962fe7": {
+			"id": "647eea65098f070019962fe7",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
